c6bfa5b04093280f2f6436f92f950a05bb0fec7f
texascrewwebmaster <texascrewwebmaster@gmail.com>
2014-04-26 20:55:25 -0700

    f

---
+++ redirect.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+Your message has been sent! Redirecting to the home page....
+
+If you are not redirected click <a href="http://texas-crew.github.io"> here</a>.
+
+<?php
+header('Refresh: 3; url=index.htm');
+
+?> 
+</html>
