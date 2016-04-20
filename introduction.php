e48b0e8bd09412673dd9ca65719af991776b2767
nicholas-wong <nicholas.wong@utexas.edu>
2016-04-20 12:01:19 -0500

    try

---
+++ introduction.php
@@ -0,0 +1,42 @@
+
+hr {
+	height: 2px;
+	border-style: solid;
+	color: #de7008;
+	background-color: #df7109;
+	/*for Firefox and Opera*/
+	margin-top: 10px;
+	border: none;
+}
+
+div {
+	padding: 3px;
+	border: 1px solid black;
+}
+#containerfullwidth {
+	width: 100%;
+	padding: 0;
+}
+.container1 {
+	float: left;
+	width: 645px;
+	margin: -20px 0 0 15px;
+	border: none;
+	padding: 0;
+} /*if the window is not wide enough this will flip down below and cover side icons when scrolling*/
+
+.container2 {
+	width: 730px;
+	margin: auto;
+	border: none;
+	padding: 0;
+}
+#container3 {
+	width: 730px;
+	margin-left: auto;
+	margin-right: auto;
+	border: none;
+	padding: 0 20px;
+	margin-top: 20px;
+	margin-bottom: 40px;
+}
